fa11adf33ecfc0ba20044eb65fc50e1ca5e951a1
lu0601 <50568129+lu0601@users.noreply.github.com>
2019-05-13 14:28:27 +0800

    Create uva 11799 - Horror Dash.cpp

---
+++ uva 11799 - Horror Dash.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    FILE *fp = fopen("test.txt","w");
+    if(fp==0)
+    {
+        printf("Cannot open test.txt.\n");
+        return 0;
+    }
+    int data[10] = {};
+    data[6] = 11;
+    int data_set=0;
+    scanf("%d",&data_set);
+    for(int i=0;i<data_set;i+=1)
+    {
+        int data_num=0;
+        scanf("%d",&data_num);
+        int data[data_num]={};
+        for(int j=0;j<data_num;j+=1)
+        {
+            scanf("%d",&data[j]);
+            //printf("%d",data[i]);
+            //printf("Case %d:%d\n",data_set+1,*max_element(data,data+data_num));
+        }
+        printf("Case %d: %d\n",i+1,*max_element(data,data+data_num));
+    }
+    return 0;
+}
